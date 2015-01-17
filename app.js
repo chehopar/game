f54f3e4898868c6a88363b587b1a057b4b77b1df
Mushex Antaranian <mushex@antaranian.com>
2015-01-17 17:58:03 +0400

    Move auth. logic to separate file

--- app.js
+++ app.js
@@ -8,11 +8,6 @@ var morgan = require('morgan'),
     cookieParser = require('cookie-parser'),
     session = require('express-session');
 
-var passport = require('passport');
-
-var LocalStrategy   = require('passport-local').Strategy;
-
-
 // instantiate app
 var app = express();
 
@@ -42,54 +37,7 @@ app.use(express.static('public'));
 // setup view rendering
 require('./core/engine.js')(app);
 
-// Passport integration
-
-// dummy user
-var user = {
-        id: 1,
-        email: 'admin@admin',
-        password: 'password',
-        name: 'Anna Sedokova'
-    };
-
-var users = { 1: user };
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
-});
-
-// used to deserialize the user
-passport.deserializeUser(function(id, done) {
-    var u = users[id];
-
-    if (!u)
-        return done(Error('no user'));
-
-    return done(null, u);
-});
-
-passport.use('local-login', new LocalStrategy({
-    usernameField : 'email',
-    passwordField : 'password',
-    passReqToCallback : true
-}, function (req, email, password, done){
-    if (user.email == 'email' && user.password == 'password')
-        return done(null, user);
-
-    return done(null, false);
-}));
-
-app.get('/login', function (req, res) {
-    res.render('login.html');
-});
-
-app.post('/login', passport.authenticate('local-login', {
-    successRedirect : '/profile', // redirect to the secure profile section
-    failureRedirect : '/login' // redirect back to the signup page if there is an error
-}));
+require('./core/authentication.js')(app);
 
 
 app.get('/', function (req, res) {
---
+++ core/authentication.js
@@ -0,0 +1,55 @@
+var passport = require('passport');
+
+var LocalStrategy   = require('passport-local').Strategy;
+
+// Passport integration
+
+// dummy user
+var user = {
+        id: 1,
+        email: 'admin@admin',
+        password: 'password',
+        name: 'Anna Sedokova'
+    };
+
+var users = { 1: user };
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+// used to deserialize the user
+passport.deserializeUser(function(id, done) {
+    var u = users[id];
+
+    if (!u)
+        return done(Error('no user'));
+
+    return done(null, u);
+});
+
+passport.use('local-login', new LocalStrategy({
+    usernameField : 'email',
+    passwordField : 'password',
+    passReqToCallback : true
+}, function (req, email, password, done){
+    if (user.email == email && user.password == password)
+        return done(null, user);
+
+    return done(null, false);
+}));
+
+module.exports = function (app) {
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    app.get('/login', function (req, res) {
+        res.render('login.html');
+    });
+
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login' // redirect back to the signup page if there is an error
+    }));
+};
+
