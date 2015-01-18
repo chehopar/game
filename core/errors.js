4e1ef9c92b3e747d85db9ef669737f7e2506ca12
Mushex Antaranian <mushex@antaranian.com>
2015-01-18 05:43:20 +0400

    Handle 404 errors

--- core/errors.js
+++ core/errors.js
@@ -1,16 +1,27 @@
 var qs = require('querystring');
 
+var Miscue = require('decanat-miscue');
+
 var app = require('../app.js');
+
+app.use(function(req, res, next){
+    var err = Miscue(404, { url: req.url });
+    next(err);
+});
+
 
 app.use(function(err, req, res, next){
     if (err.code == 401)
         return authRequired(err, req, res);
+    if (err.code == 404)
+        return notFound(err, req, res);
 
     res.status(500).send('Something broke!');
 });
 
 /**
  * Promt user login.
+ *
  * @param  {Miscue}     err
  * @param  {Request}    req
  * @param  {Response}   res
@@ -19,4 +30,17 @@ app.use(function(err, req, res, next){
 function authRequired (err, req, res) {
     var url = '/login?' + qs.stringify(err.data);
     res.redirect(url);
+}
+
+
+function notFound (err, req, res) {
+    res.status(404);
+
+    if (req.accepts('html'))
+        return res.render('errors/404.html', err.data);
+    if (req.accepts('json'))
+        return res.send(err.data);
+
+    res.type('txt')
+        .send('Not found');
 }
