e8033c2fa02fe3ec633d19c4b6f8214abd12d789
Mushex Antaranian <mushex@antaranian.com>
2015-01-18 16:17:11 +0400

    Use Redis as session store

--- core/session.js
+++ core/session.js
@@ -1,10 +1,16 @@
 var cookieParser = require('cookie-parser'),
     session = require('express-session');
 
+var redisdb = require('../transports/redis.js');
+
+var RedisStore = require('connect-redis')(session)
+
 var app = require('../app.js');
 
-var config = require('../config');
+var options = require('../config').session;
+
+options.store = new RedisStore({ client: redisdb });
 
 app.use(cookieParser());
 
-app.use(session(config.session));
+app.use(session(options));
